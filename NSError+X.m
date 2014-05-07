75fd1f09a80d473a6881f2e0f35d19917ae9037d
Petr Korolev <sky4winder@gmail.com>
2014-05-08 01:48:50 +0300

    add alert view for NSError

---
+++ NSError+X.m
@@ -0,0 +1,27 @@
+//
+// Created by Petr Korolev on 08/05/14.
+// Copyright (c) 2014 Anthony Shoumikhin. All rights reserved.
+//
+
+#import "NSError+X.h"
+
+
+@implementation NSError (X)
+
+-(void)showAlertView
+{
+    [self showAlertViewWithTitle:@"Error" andMessage:self.localizedFailureReason ?: self.localizedDescription];
+}
+
+-(void)showAlertViewWithTitle:(NSString *)title andMessage:(NSString *)message
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertView* alertView = [[UIAlertView alloc] initWithTitle:title
+                                                            message:message
+                                                           delegate:nil
+                                                  cancelButtonTitle:NSLocalizedString(@"OK", nil) otherButtonTitles:nil, nil];
+        [alertView show];
+    });
+}
+
+@end
