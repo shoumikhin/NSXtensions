6eb02274c90f90a17192561449754dc6e23308f2
Petr Korolev <sky4winder@gmail.com>
2014-05-08 01:48:50 +0300

    add alert view for NSError

---
+++ NSError+X.h
@@ -0,0 +1,12 @@
+//
+// Created by Petr Korolev on 08/05/14.
+// Copyright (c) 2014 Anthony Shoumikhin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSError (X)
+
+- (void)showAlertView;
+
+@end
