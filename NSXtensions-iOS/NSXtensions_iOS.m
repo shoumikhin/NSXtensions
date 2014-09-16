50bc496c6f2af6fb7714f3095bf372246126cb82
Petr Korolev <sky4winder@gmail.com>
2014-09-16 14:51:18 +0300

    add iOS static library

---
+++ NSXtensions-iOS/NSXtensions_iOS.m
@@ -0,0 +1,13 @@
+//
+//  NSXtensions_iOS.m
+//  NSXtensions-iOS
+//
+//  Created by Petr Korolev on 16/09/14.
+//  Copyright (c) 2014 Anthony Shoumikhin. All rights reserved.
+//
+
+#import "NSXtensions_iOS.h"
+
+@implementation NSXtensions_iOS
+
+@end
