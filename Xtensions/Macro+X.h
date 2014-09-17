32a83ac395d199f51c33d4d6f7b7f328b7700595
Petr Korolev <sky4winder@gmail.com>
2014-09-17 16:29:03 +0300

    cleanup

--- Xtensions/Macro+X.h
+++ Xtensions/Macro+X.h
@@ -8,27 +8,27 @@
 #define _QUOTE_(__text__) #__text__
 
 /**
- TODO(message)
+TODO(message)
 
- A macro to mark a line of code with a "TODO" prefixed compiler warning.
- */
+A macro to mark a line of code with a "TODO" prefixed compiler warning.
+*/
 
 #define TODO(_text_) _Pragma(_QUOTE_(message("TODO: "_text_)))
 
 /**
- FIXME(message)
+FIXME(message)
 
- A macro to mark a line of code with a "FIXME" prefixed compiler warning.
- */
+A macro to mark a line of code with a "FIXME" prefixed compiler warning.
+*/
 
 #define FIXME(_text_) _Pragma(_QUOTE_(message("FIXME: "_text_)))
 
 /**
- SHOW_ALERT(title, message, delegate, cancelButtonTitle, ...)
+SHOW_ALERT(title, message, delegate, cancelButtonTitle, ...)
 
- A macro to create and show a UIAlertView with the given title, message,
- deleagate and arbitrary number of buttons with specified titles.
- */
+A macro to create and show a UIAlertView with the given title, message,
+deleagate and arbitrary number of buttons with specified titles.
+*/
 
 #define SHOW_ALERT(_title_, _message_, _delegate_, _cancelButtonTitle_, ...) \
 ({ \
@@ -37,10 +37,10 @@
 })
 
 /**
- SYNTHESIZE_STATIC_PROPERTY(type, name, ...)
+SYNTHESIZE_STATIC_PROPERTY(type, name, ...)
 
- A macro to rapidly implement a read-only static property of a given type and name.
- */
+A macro to rapidly implement a read-only static property of a given type and name.
+*/
 
 #define SYNTHESIZE_STATIC_PROPERTY(_type_, _name_, ...) \
 + (_type_)_name_ \
@@ -59,19 +59,19 @@
 }
 
 /**
-    SYNTHESIZE_SINGLETON_FOR_CLASS(classname)
+SYNTHESIZE_SINGLETON_FOR_CLASS(classname)
 
-    A macro to synthesize singleton boilerplate code for a given class.
-    Place it under the @implementation section of the class.
-    A single instance by default is accessible via `shared` method.
-    At the first time it calls a default `init` constructor and creates an
-    instance. Each next time the same instance is returned.
- */
+A macro to synthesize singleton boilerplate code for a given class.
+Place it under the @implementation section of the class.
+A single instance by default is accessible via `shared` method.
+At the first time it calls a default `init` constructor and creates an
+instance. Each next time the same instance is returned.
+*/
 
 #import <objc/runtime.h>
 
 #if __has_feature(objc_arc)
-    #define _SYNTHESIZE_SINGLETON_RETAIN_METHODS_
+#define _SYNTHESIZE_SINGLETON_RETAIN_METHODS_
 #else
     #define _SYNTHESIZE_SINGLETON_RETAIN_METHODS_ \
 - (instancetype)retain \
@@ -119,8 +119,8 @@ _SYNTHESIZE_SINGLETON_RETAIN_METHODS_
 #define SYNTHESIZE_SINGLETON_FOR_CLASS(_classname_) _SYNTHESIZE_SINGLETON_FOR_CLASS_WITH_ACCESSOR_(_classname_, shared)
 
 /**
- *  Several macros that might be handy to detect if the device is an iPhone, iPad, or iPhone5
- */
+*  Several macros that might be handy to detect if the device is an iPhone, iPad, or iPhone5
+*/
 
 #define IS_WIDESCREEN ([[UIScreen mainScreen] bounds].size.height == 568)
 #define IS_RETINA ([[UIScreen mainScreen] scale] == 2.0)
@@ -129,18 +129,18 @@ _SYNTHESIZE_SINGLETON_RETAIN_METHODS_
 #define IS_IPHONE_5 (IS_IPHONE && IS_WIDESCREEN)
 
 /**
- *  Determining the orientation of the device
- */
-#define LANDSCAPE UIInterfaceOrientationIsLandscape(self.interfaceOrientation)
-#define LANDSCAPE_RIGHT [UIDevice currentDevice].orientation == UIDeviceOrientationLandscapeLeft
-#define LANDSCAPE_LEFT [UIDevice currentDevice].orientation == UIDeviceOrientationLandscapeRight
-#define PORTRAIT UIInterfaceOrientationIsPortrait(self.interfaceOrientation)
-#define PORTRAIT_REVERSE [UIDevice currentDevice].orientation == UIDeviceOrientationPortraitUpsideDown
+*  Determining the orientation of the device
+*/
+#define DEVICE_ORIENTATION [UIDevice currentDevice].orientation
+#define LANDSCAPE           UIInterfaceOrientationIsLandscape(DEVICE_ORIENTATION)
+#define LANDSCAPE_RIGHT     DEVICE_ORIENTATION == UIDeviceOrientationLandscapeLeft
+#define LANDSCAPE_LEFT      DEVICE_ORIENTATION == UIDeviceOrientationLandscapeRight
+#define PORTRAIT            UIInterfaceOrientationIsPortrait(DEVICE_ORIENTATION)
+#define PORTRAIT_REVERSE    DEVICE_ORIENTATION == UIDeviceOrientationPortraitUpsideDown
 
 /**
- *  Programmatically detect which iOS version is device running on:
- */
-
+*  Programmatically detect which iOS version is device running on:
+*/
 #define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
 #define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
 #define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
